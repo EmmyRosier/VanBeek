e8e17c463e646c685833d7126467a1e12a714654
Anny Oganesiants <533991@student.fontys.nl>
2026-02-24 11:52:35 +0100

    Onnodige kolommen droppen + Begin streamlit dashboard

--- code.py
+++ code.py
@@ -5,18 +5,31 @@ import numpy as np
 st.set_page_config(layout="wide")
 
 st.title("📊 Voorspellingsmodel")
+st.caption("Dit is een vergelijkbaarheid zoekmachine / aanbevelingstool.")
 
+<<<<<<< Updated upstream
 #Sidebar
+=======
+# Sidebar
+>>>>>>> Stashed changes
 st.sidebar.header("📂 Upload je Excel bestand")
 uploaded_file = st.sidebar.file_uploader(
     "Database_testproducten", 
     type=["xlsx"]
 )
 
+def load_data(uploaded_file: st.runtime.uploaded_file_manager.UploadedFile) -> pd.DataFrame:
+    name = uploaded_file.name.lower()
+    if name.endswith(".xlsx") or name.endswith(".xls"):
+        return pd.read_excel(uploaded_file)
+    if name.endswith(".csv"):
+        return pd.read_csv(uploaded_file)
+    raise ValueError("Upload een .xlsx, .xls of .csv bestand.")
+
 if uploaded_file:
-
-    df = pd.read_excel(uploaded_file)
-
+    df = load_data(uploaded_file)
+    drop_kol = ["Opmerking", "Getest door", "Foto's", "Microscoop", "Filmpjes", "Klant", "Test", "Datum", "Nr.", "Order"]
+    df = df.drop(columns = [k for k in drop_kol if k in df.columns])
     st.subheader("Data preview")
     st.dataframe(df.head())
 
