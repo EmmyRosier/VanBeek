1c3c070e131d98da8095a02056482deda5b1b0f2
EmmyRosier <539088@student.fontys.nl>
2026-03-17 14:36:54 +0100

    Tweede optie toegevoegd en kleine aanpassingetjes

--- code.py
+++ code.py
@@ -6,8 +6,8 @@ import pandas as pd
 import numpy as np
 
 st.set_page_config(layout="wide")
-st.image("vanBeekLogoPNG.png", width=200)
-st.title("Voorspellingsmodel")
+st.image("vanBeekLogoPNG.png", width=150)
+st.title("Overeenkomende producten")
 
 # Sidebar upload
 st.sidebar.header("📂 Upload Excel bestand")
@@ -297,18 +297,8 @@ if st.button("Toon resultaten"):
         st.write(styled_df.to_html(escape=False), unsafe_allow_html=True)
 
         # Opmerkingen tonen
-        # Als er alleen cijfers staan in de kolom, wordt de maaklink functie toegepast zodat het ook een link is binnen opmerkingen en niet alleen resultaten.
-        
         if "Opmerking" in beste_matches.columns:
             for i, row in enumerate(beste_matches.itertuples(), 1):
                 opm = getattr(row, "Opmerking", None)
-
                 if pd.notna(opm) and str(opm).strip() != "":
-                    opm_str = str(opm).strip()
-
-                    # Alleen als de opmerking uitsluitend uit cijfers bestaat:
-                    if opm_str.isdigit():
-                        opm_html = maak_link(opm_str)
-                        st.markdown(f"**Opmerking bij match {i}:** {opm_html}", unsafe_allow_html=True)
-                    else:
-                        st.info(f"Opmerking bij match {i}: {opm_str}")
+                    st.info(f"Opmerking bij match {i}: {opm}")
